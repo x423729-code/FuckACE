63ca51a1ff31bab65fef6c25bdbb4b8823d792e5
ByteBox <x423729@gmail.com>
2026-03-31 02:17:40 +0800

    feat: 提交Java移植版初始代码

--- src/main/java/org/example/service/Launcher.java
+++ src/main/java/org/example/service/Launcher.java
@@ -3,6 +3,6 @@ package org.example.service;
 public class Launcher {
     public static void main(String[] args) {
         // 这一步是关键：由一个不继承 Application 的类来启动
-        AppMain.main(args);
+        org.example.service.AppMain.main(args);
     }
 }
